2abc9bfc34d5b7f87eef3063e932faac472f6365
sohaildani <mohdsohaildk@gmail.com>
2016-09-01 07:34:29 +0530

    App changed

--- app/src/main/java/com/sohaildani/medicoalert/PhotosFragment.java
+++ app/src/main/java/com/sohaildani/medicoalert/PhotosFragment.java
@@ -1,10 +1,16 @@
 package com.sohaildani.medicoalert;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.location.GpsStatus;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class PhotosFragment extends Fragment {
 	
@@ -15,7 +21,19 @@ public class PhotosFragment extends Fragment {
             Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.fragment_photos, container, false);
-         
+        ImageView i=(ImageView)rootView.findViewById(R.id.image);
+
+
+        i.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                next();
+            }
+        });
         return rootView;
+    }
+    public void next(){
+        //Intent intent=new Intent(getActivity(),MainActivity2.class);
+        //startActivity(intent);
     }
 }
